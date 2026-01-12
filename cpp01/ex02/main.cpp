b2467f3a00742d926b95fe153d39dc7a3bb613b8
carowann <cwannhed@student.42firenze.it>
2026-01-12 11:30:28 +0100

    done with ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 11:07:00 by cwannhed          #+#    #+#             */
+/*   Updated: 2026/01/12 11:29:54 by cwannhed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main()
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "Memory address of the string variable: " << &string << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF " << &stringREF << std::endl;
+	std::cout << "\nValue of the string variable: " << string << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
