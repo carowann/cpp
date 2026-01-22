5992e05409964581ea7a26c3c68bf097ab26e644
carowann <cwannhed@student.42firenze.it>
2026-01-22 12:04:06 +0100

    finished cpp01

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,21 +6,21 @@
 /*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 11:07:00 by cwannhed          #+#    #+#             */
-/*   Updated: 2026/01/12 11:29:54 by cwannhed         ###   ########.fr       */
+/*   Updated: 2026/01/22 11:38:22 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 
-int	main()
+int	main(void)
 {
 	std::string	string = "HI THIS IS BRAIN";
 	std::string *stringPTR = &string;
 	std::string &stringREF = string;
 
 	std::cout << "Memory address of the string variable: " << &string << std::endl;
-	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringPTR: " << &stringPTR << std::endl;
 	std::cout << "Memory address held by stringREF " << &stringREF << std::endl;
 	std::cout << "\nValue of the string variable: " << string << std::endl;
 	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
