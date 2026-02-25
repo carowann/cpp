ce01ceb03eb8a29f870c1514681c5e23155c7309
carowann <cwannhed@student.42firenze.it>
2026-02-25 17:54:12 +0100

    finished

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 17:59:22 by cwannhed          #+#    #+#             */
-/*   Updated: 2026/02/24 18:25:03 by cwannhed         ###   ########.fr       */
+/*   Updated: 2026/02/25 14:50:31 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,20 @@
 # include <cstddef>
 # include <iostream>
 
+# define BOLD	"\033[1m"
+# define RESET	"\033[0m"
+
 template <typename T, typename F>
 void	iter(T *array, size_t const len, F func) {
 	for (size_t i = 0; i < len; i ++)
 		func(array[i]);
 }
 
-template <typename T>
-void	print(T const &a) {
-	std::cout << a << std::endl;
+template <typename T, typename F>
+void iter(T const *array, size_t const len, F func) {
+	for (size_t i = 0; i < len; i++)
+		func(array[i]);
 }
+
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,18 +6,58 @@
 /*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 17:59:12 by cwannhed          #+#    #+#             */
-/*   Updated: 2026/02/24 18:23:54 by cwannhed         ###   ########.fr       */
+/*   Updated: 2026/02/25 14:53:43 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int	main() {
-	int	intArray[10];
-	for (size_t i = 0; i < 10; i++) {
-		intArray[i] = i;
-	}
-	iter(intArray, 10, print<int>);
-	return (0);
+void	testHeader(std::string const &testName) {
+	std::cout << BOLD << "\n==================== " << testName << " ====================" << RESET << std::endl;
+}
 
+template <typename T>
+void	print(T const &a) {
+	std::cout << a << std::endl;
+}
+
+template <typename T>
+void	doubleValue(T &a) {
+	a *= 2;
+}
+
+template <typename T>
+void	printUpperCase(T const &a) {
+	(void)a;
+	std::cout << "not a string" << std::endl;
+}
+
+template <>
+void	printUpperCase<std::string>(std::string const &a) {
+	for (size_t i = 0; i < a.size(); i++)
+		std::cout << static_cast<char>(std::toupper(a[i]));
+	std::cout << std::endl;
+}
+
+int	main() {
+	testHeader("Int array - print");
+	int	intArray[5] = {1, 2, 3, 4, 5};
+	iter(intArray, 5, print<int>);
+
+	testHeader("Int array - double value (non-const function)");
+	iter(intArray, 5, doubleValue<int>);
+	iter(intArray, 5, print<int>);
+
+	testHeader("String array - print");
+	std::string	stringArray[3] = {"hello", "fortytwo", "world"};
+	iter(stringArray, 3, print<std::string>);
+
+	testHeader("String array - uppercase (custom function)");
+	iter(stringArray, 3, printUpperCase<std::string>);
+
+	testHeader("Const int array - print");
+	const int	constArray[4] = {10, 20, 30, 40};
+	iter(constArray, 4, print<int>);
+
+	return (0);
 }
